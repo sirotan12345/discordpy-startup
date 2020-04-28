664e97d55655fa7b3c2cf3270211ac0d58a1ac00
keiji shima(sirotan) <sample@sirotan.com>
2020-04-29 00:39:51 +0900

    local確認用bot tokenは削除

--- src/include/buildenv.py
+++ src/include/buildenv.py
@@ -1,9 +1,10 @@
 import os
 
-# 実運用のHeroku上はDISCORD_BOT_TOKENが定義されている。
-# ローカル動作確認時はDISCORD_BOT_TOKENが未定義なので、確認用BOTのtokenを指定する。
+# 実運用のHeroku上ではDISCORD_BOT_TOKENが定義されているため、それを使用する。
+# ローカル動作確認時は別途確認用BOTのtokenを指定するが、token文字列はpush出来ないため空白にしてる。
 try:
     DISCORD_BOT_TOKEN
     token = os.environ['DISCORD_BOT_TOKEN']
 except NameError:
-    token = "NzA0NzEyMDcxNjE4NDk0NjQ0.XqhK8Q.0kY9QT7MIlmh0XMqIGDbk3X2zUY"
+    # ここにtoken文字列を入れる。
+    token = ""
